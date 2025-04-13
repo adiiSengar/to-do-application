eef1a9197a93e842c67f10d5fa3731947b68b208
payal271 <pg27012007@gmail.com>
2025-04-13 23:34:53 +0530

    Moved deleteTask() JavaDoc to correct location

--- src/main/java/io/github/adiisengar/todoapplication/to_do_app/TaskService.java
+++ src/main/java/io/github/adiisengar/todoapplication/to_do_app/TaskService.java
@@ -4,30 +4,63 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import java.util.List;
 import java.util.Optional;
+/**
+ * Service class that contains business logic for managing tasks.
+ */
+
 @Service
 
 public class TaskService {
     @Autowired
     private TaskRepository taskRepository;
+    /**
+     * Get a list of all tasks.
+     * @return List of all tasks from the database
+     */
 
- public List<Task> getAllTasks() {
+
+    public List<Task> getAllTasks() {
      return taskRepository.findAll();
  }
- public Optional<Task> getTaskById(Long id) {
+    /**
+     * Get a single task by its ID.
+     * @param id ID of the task to retrieve
+     * @return Optional containing the task if found, otherwise empty
+     */
+
+    public Optional<Task> getTaskById(Long id) {
 
      return taskRepository.findById(id);
  }
- public Task saveTask(Task task) {
+    /**
+     * Save a new task to the database.
+     * @param task Task object to save
+     * @return Saved task object with generated ID
+     */
+
+    public Task saveTask(Task task) {
      return taskRepository.save(task);
  }
- public Task updateTask(Long id, Task taskDetails) {
+    /**
+     * Update an existing task using its ID.
+     * @param id ID of the task to update
+     * @param taskDetails New task data to update
+     * @return Updated task object if ID exists, otherwise null
+     */
+
+    public Task updateTask(Long id, Task taskDetails) {
 
      if(taskRepository.existsById(id)){
          taskDetails.setId(id);
          return null;
-
      }
- return taskRepository.save(taskDetails);}
+        /**
+         * Delete a task using its ID.
+         * @param id ID of the task to delete
+         * @return true if task was deleted, false if task does not exist
+         */
+
+        return taskRepository.save(taskDetails);}
      public boolean deleteTask(Long id){
          if(taskRepository.existsById(id)){
              taskRepository.deleteById(id);
