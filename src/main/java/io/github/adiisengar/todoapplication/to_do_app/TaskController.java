b73f62172017e788a0b8708dc134783837812656
adiiSengar <adityasinghsengar750@gmail.com>
2025-04-13 16:23:16 +0530

    errors handle

--- src/main/java/io/github/adiisengar/todoapplication/to_do_app/TaskController.java
+++ src/main/java/io/github/adiisengar/todoapplication/to_do_app/TaskController.java
@@ -6,41 +6,37 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 import java.util.Optional;
 
-public class TaskController {
-
 @RestController
 @RequestMapping("/api/tasks")
-public class Task {
+public class TaskController {
 
     @Autowired
-    private TaskService taskService taskService;
+    private TaskService taskService;
 
-    //Get all Tasks
     @GetMapping
     public List<Task> getAllTasks() {
         return taskService.getAllTasks();
     }
-//Get task by ID
+
     @GetMapping("/{id}")
     public Optional<Task> getTask(@PathVariable Long id) {
         return taskService.get TaskByID(id);
     }
-    // Create a new task
+
     @PutMapping
     public Task updateTask(@RequestBody Task task) {
         return taskService.addTask(task);
     }
 
-    //update a task
+
     @PutMapping("{id}")
     public Task updateTask(@PathVariable Long id, @RequestBody Task task) {
         return taskService.updateTask(id task);
     }
 
-    // Delete a task
+
     @DeleteMapping("/{id}")
     public boolean deleteTask(@PathVariable Long id) {
     return taskService.deleteTask(id);
     }
-
 }
