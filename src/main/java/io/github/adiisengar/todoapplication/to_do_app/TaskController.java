50006aef1a09633e00b3b20300e533d62b2c8861
Shivani11422 <shivanishrivastav6740@gmail.com>
2025-04-13 16:14:30 +0530

    update Task Controller

--- src/main/java/io/github/adiisengar/todoapplication/to_do_app/TaskController.java
+++ src/main/java/io/github/adiisengar/todoapplication/to_do_app/TaskController.java
@@ -1,4 +1,52 @@
 package io.github.adiisengar.todoapplication.to_do_app;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
 public class TaskController {
+package com.username;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
+
+@RestController
+@RequestMapping("/api/tasks")
+public class Task {
+
+    @Autowired
+    private TaskService taskService taskService;
+
+    //Get all Tasks
+    @GetMapping
+    public List<Task> getAllTasks() {
+        return taskService.getAllTasks();
+    }
+//Get task by ID
+    @GetMapping("/{id}")
+    public Optional<Task> getTask(@PathVariable Long id) {
+        return taskService.get TaskByID(id);
+    }
+    // Create a new task
+    @PutMapping
+    public Task updateTask(@RequestBody Task task) {
+        return taskService.addTask(task);
+    }
+
+    //update a task
+    @PutMapping("{id}")
+    public Task updateTask(@PathVariable Long id, @RequestBody Task task) {
+        return taskService.updateTask(id task);
+    }
+
+    // Delete a task
+    @DeleteMapping("/{id}")
+    public boolean deleteTask(@PathVariable Long id) {
+    return taskService.deleteTask(id);
+    }
+
+
+
 }
