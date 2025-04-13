8e6b67672d794fa645d0449f2dfc0ccf30e406fe
adiiSengar <adityasinghsengar750@gmail.com>
2025-04-13 19:23:14 +0530

    equals, hashcode, tostring added

--- src/main/java/io/github/adiisengar/todoapplication/to_do_app/Task.java
+++ src/main/java/io/github/adiisengar/todoapplication/to_do_app/Task.java
@@ -2,6 +2,8 @@ package io.github.adiisengar.todoapplication.to_do_app;
 
 import jakarta.persistence.*;
 
+import java.util.Objects;
+
 @Entity
 @Table(name = "tasks")
 public class Task {
@@ -51,5 +53,27 @@ public class Task {
 
     public void setCompleted(boolean completed) {
         this.completed = completed;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) return false;
+        Task task = (Task) o;
+        return completed == task.completed && Objects.equals(id, task.id) && Objects.equals(title, task.title) && Objects.equals(description, task.description);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, title, description, completed);
+    }
+
+    @Override
+    public String toString() {
+        return "Task{" +
+                "id=" + id +
+                ", title='" + title + '\'' +
+                ", description='" + description + '\'' +
+                ", completed=" + completed +
+                '}';
     }
 }
--- src/main/java/io/github/adiisengar/todoapplication/to_do_app/TaskNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package io.github.adiisengar.todoapplication.to_do_app;
-
-public class TaskNotFoundException extends RuntimeException {
-    public TaskNotFoundException(String message) {
-        super(message);
-    }
-}
